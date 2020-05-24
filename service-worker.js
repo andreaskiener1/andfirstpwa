c901f9579ca93a7e57467ab1e38339e36b8fa491
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-24 22:39:38 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -23,11 +23,10 @@ const CACHE_NAME = 'static-cache-v1';
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
   '/',
-  '/public',
-  '/public/index.html',
-  '/public/service-worker.js',
-  '/public/favicon.ico',
-  '/public/manifest.json'
+  'index.html',
+  'service-worker.js',
+  'favicon.ico',
+  'manifest.json'
 ];
 
 
