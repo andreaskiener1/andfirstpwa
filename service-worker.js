eb7569b0fceb387d7f62440e4d39526278566b93
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-26 16:44:26 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -15,10 +15,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License
  */
-'use strict';
+//'use strict';
 
 // CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v1'
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
@@ -29,9 +29,33 @@ const FILES_TO_CACHE = [
   '/manifest.json',
   '/caesar192.png',
   '/caesar512.png'
-];
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(cache =>
+        cache.addAll(FILES_TO_CACHE)
+      )
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        //we found an entry in the cache!
+        return response
+      }
+      return fetch(event.request)
+    })
+  )
+})
 
 
+
+/*
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
   // CODELAB: Precache static resources here.
@@ -54,7 +78,7 @@ self.addEventListener('install', function(event) {
       })
   );
 });
-*/
+
 self.addEventListener('activate', (evt) => {
   console.log('[ServiceWorker] Activate');
   // CODELAB: Remove previous cached data from disk.
@@ -70,7 +94,7 @@ self.addEventListener('activate', (evt) => {
   );
   self.clients.claim();
 });
-
+*/
 /*
 self.addEventListener('fetch', (evt) => {
   console.log('[ServiceWorker] Fetch', evt.request.url);
