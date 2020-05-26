bb0a1d35f3e9373fefe51c8cbec76fc124cb36c9
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-26 16:01:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var path = require('path')
 var app = express()
 app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
-var dateipfad = path.join(__dirname, 'public', 'index.html')
+//var dateipfad = path.join(__dirname, 'public', 'index.html')
+var dateipfad = path.join(__dirname, 'index.html')
  
 app.get('/', function(request, response) {
     response.sendFile(dateipfad)
