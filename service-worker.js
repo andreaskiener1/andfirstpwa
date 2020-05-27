593e1a1bf3a7691db818eb3bbc385b773aa0b8ec
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-05-27 07:58:41 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -31,41 +31,9 @@ const FILES_TO_CACHE = [
   '/caesar512.png'
 ]
 
-self.addEventListener("install", function(event) {
-  console.log('WORKER: install event in progress.');
-  event.waitUntil(
-    /* The caches built-in is a promise-based API that helps you cache responses,
-       as well as finding and deleting them.
-    */
-    caches
-      /* You can open a cache by name, and this method returns a promise. We use
-         a versioned cache name here so that we can remove old cache entries in
-         one fell swoop later, when phasing out an older service worker.
-      */
-      .open(CACHE_NAME)
-      .then(function(cache) {
-        /* After the cache is opened, we can fill it with the offline fundamentals.
-           The method below will add all resources we've indicated to the cache,
-           after making HTTP requests for each of them.
-        */
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/service-worker.js',
-          '/favicon.ico',
-          '/manifest.json',
-          '/caesar192.png',
-          '/caesar512.png'
-          
-        ]);
-      })
-      .then(function() {
-        console.log('WORKER: install completed');
-      })
-  );
-});
 
-/*
+
+
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
   // CODELAB: Precache static resources here.
